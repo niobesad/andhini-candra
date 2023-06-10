d939b3b466240524e8b98b8240e96684c6adc440
niobesad <40653897+niobesad@users.noreply.github.com>
2023-06-10 23:50:01 +0700

    Initial commit

--- js/menikah.js
+++ js/menikah.js
@@ -1,6 +1,29 @@
 // Get that hamburger menu cookin' //
+var modal = document.getElementById("modalWeddingGift");
+var welcomeModal = document.getElementById("welcomeModal");
+var containerGallery = document.getElementById("containerGallery")
+var containerClosing = document.getElementById("containerClosing")
+var ythLabel = document.getElementById('yth')
+var toLabel = document.getElementById('to')
+var inviteLabel = document.getElementById('invite')
+var myAudio = document.getElementById("my_audio");
 
-document.addEventListener("DOMContentLoaded", function() {
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString)
+
+if (urlParams.get('to') != null) {
+  toLabel.innerHTML = urlParams.get('to')
+} else {
+  ythLabel.innerHTML = ""
+  inviteLabel.innerHTML = ""
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  //open welcome modal
+  welcomeModal.style.display = 'block'
+  // if (urlParams.get('to') != null) {
+  // }
+
   // Get all "navbar-burger" elements
   var $navbarBurgers = Array.prototype.slice.call(
     document.querySelectorAll(".navbar-burger"),
@@ -9,8 +32,8 @@ document.addEventListener("DOMContentLoaded", function() {
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
     // Add a click event on each of them
-    $navbarBurgers.forEach(function($el) {
-      $el.addEventListener("click", function() {
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener("click", function () {
         // Get the target from the "data-target" attribute
         var target = $el.dataset.target;
         var $target = document.getElementById(target);
@@ -22,8 +45,13 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
+// play music in background
+function toggleAudio(value) {
+  return !value.checked ? myAudio.play() : myAudio.pause();
+}
+
 // Smooth Anchor Scrolling
-$(document).on("click", 'a[href^="#"]', function(event) {
+$(document).on("click", 'a[href^="#"]', function (event) {
   event.preventDefault();
   $("html, body").animate(
     {
@@ -34,7 +62,7 @@ $(document).on("click", 'a[href^="#"]', function(event) {
 });
 
 // When the user scrolls down 20px from the top of the document, show the scroll up button
-window.onscroll = function() {
+window.onscroll = function () {
   scrollFunction();
 };
 
@@ -47,11 +75,44 @@ function scrollFunction() {
 }
 
 // Preloader
-$(document).ready(function($) {
+$(document).ready(function ($) {
   $(".preloader-wrapper").fadeOut();
   $("body").removeClass("preloader-site");
 });
-$(window).load(function() {
+$(window).load(function () {
   var Body = $("body");
   Body.addClass("preloader-site");
 });
+
+function openModal() {
+  containerGallery.className = 'container is-blurred'
+  containerClosing.className = 'container is-blurred'
+  modal.style.display = 'block'
+}
+
+function closeModal() {
+  modal.style.display = 'none'
+  containerGallery.className = 'container'
+  containerClosing.className = 'container'
+}
+
+function closeWelcomeModal() {
+  welcomeModal.style.display = 'none'
+  myAudio.play()
+}
+
+function copyToClipboard(element) {
+  var value = element.innerText
+  var popup1 = document.getElementById("popupCopy1")
+  var popup2 = document.getElementById("popupCopy2")
+  element.id == 'ananda' ? popup1.classList.toggle("show") :
+    popup2.classList.toggle("show")
+
+  navigator.clipboard.writeText(value)
+    .then(() => {
+      console.log("Text copied to clipboard...")
+    })
+    .catch(err => {
+      console.log('Something went wrong', err);
+    })
+}
